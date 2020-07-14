d7fe7de80e823ae080c9c1fa3255fc5880e32ca0
Leo Kiszewski <kiszewski1999@gmail.com>
2020-07-14 20:22:58 -0300

    criado model de transacao e componentes principais

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,13 +15,41 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+   final _transactions = [
+    Transaction(
+      id: 't1',
+      name: 'Tenis',
+      value: 349.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      name: 'Mochila',
+      value: 99.90,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas pessoais'),
       ),
-      body: Center(child: Text('Versão 1.0'),),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Card(
+            elevation: 5,
+            child: Text('Grafico'),
+          ),
+          Card(
+            elevation: 5,
+            child: Text('Transações'),
+          ),
+        ],
+      ),
     );
   }
 }
