96a08b47672c31477f18beab00a04f1d5dd5665b
Leo Kiszewski <kiszewski1999@gmail.com>
2020-07-15 20:14:20 -0300

    criação e ajustes no componente transação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(ExpensesApp());
@@ -15,7 +16,7 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-   final _transactions = [
+  final _transactions = [
     Transaction(
       id: 't1',
       name: 'Tenis',
@@ -44,10 +45,34 @@ class MyHomePage extends StatelessWidget {
             elevation: 5,
             child: Text('Grafico'),
           ),
-          Card(
-            elevation: 5,
-            child: Text('Transações'),
-          ),
+          Column(
+              children: _transactions.map((tr) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(tr.name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                      Text(DateFormat('d MMM y').format(tr.date), style: TextStyle(color: Colors.grey))
+                    ],
+                  )
+                ],
+              ),
+            );
+          }).toList()),
         ],
       ),
     );
